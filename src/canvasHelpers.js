71e3ff051270c01f5da1c69f330c647a90e3c997
github username <github email address>
2019-02-21 00:38:28 -0500

    Done.

--- src/canvasHelpers.js
+++ src/canvasHelpers.js
@@ -8,27 +8,27 @@ let idx = 0
 let [sizeX, sizeY] = [95, 121]
 
 export function init() {
-  
+
   def = document.createElement("img")
   const green = document.createElement("img")
   const red = document.createElement("img")
   const yellow = document.createElement("img")
 
-  def.src = "chrome-boi.png"
+  def.src = "http://www.pngmart.com/files/2/Pokeball-Transparent-Background.png"
   green.src = "chrome-boi-green.png"
   red.src = "chrome-boi-red.png"
   yellow.src = "chrome-boi-yellow.png"
-  
+
   colors = [green, red, yellow]
 }
 
 export function drawChromeBoiAtCoords(x, y) {
-  
+
   const canvas = document.querySelector("canvas") // sloppy but we haven't introduced lifecycle methods and canvas wouldn't be rendered
   const ctx = canvas.getContext("2d")
   const rect = canvas.getBoundingClientRect()
   const [cX, cY] = [rect.left, rect.top]
-  
+
   let img
   if (cycling) {
     img = colors[idx]
@@ -36,7 +36,7 @@ export function drawChromeBoiAtCoords(x, y) {
   } else {
     img = def
   }
-  
+
   ctx.drawImage(img, x - cX - 50, y - cY - 80, sizeX, sizeY)
 }
 
